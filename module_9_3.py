f55b1b8d0e47d0cf96408e2326541fce13cb08ce
Artur <arturjan82@inbox.ru>
2024-12-03 23:43:23 +0500

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x[0])-len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+second_result = (True if len(first[i]) == len(second[i]) else False for i in range(0, len(first)))
+
+
+print(list(first_result))
+print(list(second_result))
